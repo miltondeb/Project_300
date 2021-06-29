6735b17e4a77719c28e98b9750d11a6fc8a9185a
Milton Debnath <miltondebcse@gmail.com>
2021-06-29 21:14:40 +0600

    change starting page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel New Learning
                 </div>
 
                 <div class="links">
